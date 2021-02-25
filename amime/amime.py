e3dc931ad925c5595eeb530c39f29965d23c6da2
AndrielFR <andrielkogama2@gmail.com>
2021-02-25 16:20:35 -0300

    amime: import the listen monkeypatch of pyromod

--- amime/amime.py
+++ amime/amime.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import pyromod.listen
+
 from datetime import datetime
 
 from pyrogram import Client

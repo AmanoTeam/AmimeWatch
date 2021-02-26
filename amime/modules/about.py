2086ea950016339caff53888c97bf8f52e6a074e
AndrielFR <andrielkogama2@gmail.com>
2021-02-26 20:42:45 -0300

    amime: create a about module

---
+++ amime/modules/about.py
@@ -0,0 +1,59 @@
+# MIT License
+#
+# Copyright (c) 2021 Amano Team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, Message
+from pyromod.helpers import ikb
+from typing import Union
+
+from ..amime import Amime
+
+
+@Amime.on_message(filters.cmd("about"))
+async def about_message(bot: Amime, message: Message):
+    await about_union(bot, message)
+
+
+@Amime.on_callback_query(filters.regex(r"^about"))
+async def about_callback(bot: Amime, callback: CallbackQuery):
+    await about_union(bot, callback)
+
+
+async def about_union(bot: Amime, union: Union[CallbackQuery, Message]):
+    is_callback = isinstance(union, CallbackQuery)
+    lang = union._lang
+
+    keyboard = [[(lang.collaborators_button, "about collaborators")]]
+
+    if is_callback:
+        keyboard.append([(lang.back_button, "start")])
+
+    await (union.edit_message_text if is_callback else union.reply_text)(
+        lang.about.format(
+            bot_name=bot.me.first_name,
+            github="<a href='https://github.com/AmanoTeam/AmimeWatch'>GitHub</a>",
+            channel=f"<a href='https://t.me/AmimeWatchChannel'>{lang.channel}</a>",
+            group=f"<a href='https://t.me/AmimeWatchGroup'>{lang.group}</a>",
+        ),
+        reply_markup=ikb(keyboard),
+        disable_web_page_preview=True,
+    )

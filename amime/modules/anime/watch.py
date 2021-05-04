3cc5293a285de5329ce53d5deed3883e7da4b8e5
AndrielFR <andrielkogama2@gmail.com>
2021-05-04 17:27:39 -0300

    anime: watch: show episode views

--- amime/modules/anime/watch.py
+++ amime/modules/anime/watch.py
@@ -91,6 +91,9 @@ async def anime_episode(bot: Amime, callback: CallbackQuery):
             if len(episode.notes) > 0:
                 text += f"\n\n<b>{lang.notes}</b>: <i>{episode.notes}</i>"
 
+            viewed = await Viewed.filter(item=episode.id, type="anime")
+            text += f"\n\n<b>{len(viewed)} {lang.views.lower()}</b>"
+
             previous_button = await get_previous_episode_button(
                 lang, episodes, all_episodes, anime_id, season, number, language
             )

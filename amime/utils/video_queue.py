39c6fcc14787383ef92f7f3004b6e53873920791
AndrielFR <andrielkogama2@gmail.com>
2021-05-10 15:29:55 -0300

    utils: video_queue: put a try/except in all on next function

--- amime/utils/video_queue.py
+++ amime/utils/video_queue.py
@@ -63,121 +63,121 @@ class VideoQueue(object):
         item = self.queue.get_nowait()
         id, video = item.values()
 
-        episode = await Episodes.get(id=id)
-
-        directory = f"./downloads/{random.randint(0, 9999)}/"
-        while os.path.exists(directory):
-            directory = f"./downloads/{random.randint(0, 9999)}/"
-
-        path = await self.bot.download_media(video, file_name=directory)
-        attempts = 0
-        while not bool(path):
-            attempts += 1
-            if attempts >= 3:
-                text = "<b>Error processing an episode</b>\n"
-                text += "\n<b>Anime</b>:"
-                text += f"\n    <b>ID</b>: <code>{episode.anime}</code>"
-                text += "\n\n<b>Episode</b>:"
-                if episode.season > 0:
-                    text += f"\n    <b>Season</b>: <code>{episode.season}</code>"
-                text += f"\n    <b>Number</b>: <code>{episode.number}</code>"
-                text += "\n\n<b>Error</b>:"
-                text += "\n    <b>AttemptsError</b>:"
-                text += "\n        <i>The number of download attempts has been exhausted.</i>"
-
-                await self.bot.send_message(
-                    CHATS["staff"],
-                    text,
-                )
-
-                if self.queue.empty() is False:
-                    await self.next()
-                return
-
-            path = await self.bot.download_media(
-                video, file_name=directory + video.file_name
-            )
-
-        extension = os.path.splitext(path)[1][1:].strip()
-
-        anime = await anilist.AsyncClient().get(episode.anime, "anime")
-        while anime is None:
-            anime = await anilist.AsyncClient().get(episode.anime, "anime")
-            await asyncio.sleep(5)
-
-        if isinstance(video, Document):
-            if extension == "mkv":
-                codec = None
-                softsubbed = False
-
-                proc = await asyncio.create_subprocess_shell(
-                    f'ffmpeg -i "{path}" -hide_banner',
-                    stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.STDOUT,
-                )
-                stdout = (await proc.communicate())[0]
-
-                lines = stdout.decode().lower().splitlines()
-                for line in lines:
-                    if (time := re.search(r"(\d+):(\d+):(\d+)", line)) :
-                        hours, minutes, seconds = time.groups()
-                        duration = 0
-                        duration += int(hours) * 60 * 60
-                        duration += int(minutes) * 60
-                        duration += int(seconds)
-                        video.duration = duration
-                    if (resolution := re.search(r"(\d+)x(\d+)", line)) :
-                        width, height = resolution.groups()
-                        video.width = int(width)
-                        video.height = int(height)
-                    if (codec := re.search(r"video: (\w+)", line)) :
-                        codec = codec.group(1)
-                    if re.search(r"\((\w+)\): subtitle: (\w+)", line):
-                        softsubbed = True
-
-                new_path = path.replace(".mkv", ".mp4")
-                crf = "-crf 18 " if codec == "hevc" else ""
-                if softsubbed:
-                    bitrate = (
-                        "2M"
-                        if (video.width >= 1920 and video.height >= 1080)
-                        else "950k"
-                        if (video.width >= 1280 and video.height >= 720)
-                        else "550k"
-                    )
-                    proc = await asyncio.create_subprocess_shell(
-                        f'ffmpeg -i "{path}" -vf subtitles="{path}":si=0:force_style="FontName=Trebuchet MS Bold" -c:v libx264 {crf}-pix_fmt yuv420p -b:v {bitrate} -c:a aac -map 0:v -map 0:a:0 "{new_path}" -y',
-                        stdout=asyncio.subprocess.PIPE,
-                        stderr=asyncio.subprocess.STDOUT,
-                    )
-                else:
-                    proc = await asyncio.create_subprocess_shell(
-                        f'ffmpeg -i "{path}" -c copy {crf}"{new_path}" -y',
-                        stdout=asyncio.subprocess.PIPE,
-                        stderr=asyncio.subprocess.STDOUT,
-                    )
-                await proc.communicate()
-
-                os.remove(path)
-                path = new_path
-                extension = "mp4"
-
-        thumb = path.replace(f".{extension}", ".jpg")
-        proc = await asyncio.create_subprocess_shell(
-            f'ffmpeg -ss 00:00:20 -i "{path}" -vf scale=320:-1 -vframes 1 -q:v 2 "{thumb}" -y',
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.STDOUT,
-        )
-        await proc.communicate()
-
-        if not os.path.exists(thumb):
-            thumb = (
-                await self.bot.download_media(video.thumbs[0])
-                if bool(video.thumbs) and len(video.thumbs) > 0
-                else None
-            )
-
         try:
+            episode = await Episodes.get(id=id)
+
+            directory = f"./downloads/{random.randint(0, 9999)}/"
+            while os.path.exists(directory):
+                directory = f"./downloads/{random.randint(0, 9999)}/"
+
+            path = await self.bot.download_media(video, file_name=directory)
+            attempts = 0
+            while not bool(path):
+                attempts += 1
+                if attempts >= 3:
+                    text = "<b>Error processing an episode</b>\n"
+                    text += "\n<b>Anime</b>:"
+                    text += f"\n    <b>ID</b>: <code>{episode.anime}</code>"
+                    text += "\n\n<b>Episode</b>:"
+                    if episode.season > 0:
+                        text += f"\n    <b>Season</b>: <code>{episode.season}</code>"
+                    text += f"\n    <b>Number</b>: <code>{episode.number}</code>"
+                    text += "\n\n<b>Error</b>:"
+                    text += "\n    <b>AttemptsError</b>:"
+                    text += "\n        <i>The number of download attempts has been exhausted.</i>"
+
+                    await self.bot.send_message(
+                        CHATS["staff"],
+                        text,
+                    )
+
+                    if self.queue.empty() is False:
+                        await self.next()
+                    return
+
+                path = await self.bot.download_media(
+                    video, file_name=directory + video.file_name
+                )
+
+            extension = os.path.splitext(path)[1][1:].strip()
+
+            anime = await anilist.AsyncClient().get(episode.anime, "anime")
+            while anime is None:
+                anime = await anilist.AsyncClient().get(episode.anime, "anime")
+                await asyncio.sleep(5)
+
+            if isinstance(video, Document):
+                if extension == "mkv":
+                    codec = None
+                    softsubbed = False
+
+                    proc = await asyncio.create_subprocess_shell(
+                        f'ffmpeg -i "{path}" -hide_banner',
+                        stdout=asyncio.subprocess.PIPE,
+                        stderr=asyncio.subprocess.STDOUT,
+                    )
+                    stdout = (await proc.communicate())[0]
+
+                    lines = stdout.decode().lower().splitlines()
+                    for line in lines:
+                        if (time := re.search(r"(\d+):(\d+):(\d+)", line)) :
+                            hours, minutes, seconds = time.groups()
+                            duration = 0
+                            duration += int(hours) * 60 * 60
+                            duration += int(minutes) * 60
+                            duration += int(seconds)
+                            video.duration = duration
+                        if (resolution := re.search(r"(\d+)x(\d+)", line)) :
+                            width, height = resolution.groups()
+                            video.width = int(width)
+                            video.height = int(height)
+                        if (codec := re.search(r"video: (\w+)", line)) :
+                            codec = codec.group(1)
+                        if re.search(r"\((\w+)\): subtitle: (\w+)", line):
+                            softsubbed = True
+
+                    new_path = path.replace(".mkv", ".mp4")
+                    crf = "-crf 18 " if codec == "hevc" else ""
+                    if softsubbed:
+                        bitrate = (
+                            "2M"
+                            if (video.width >= 1920 and video.height >= 1080)
+                            else "950k"
+                            if (video.width >= 1280 and video.height >= 720)
+                            else "550k"
+                        )
+                        proc = await asyncio.create_subprocess_shell(
+                            f'ffmpeg -i "{path}" -vf subtitles="{path}":si=0:force_style="FontName=Trebuchet MS Bold" -c:v libx264 {crf}-pix_fmt yuv420p -b:v {bitrate} -c:a aac -map 0:v -map 0:a:0 "{new_path}" -y',
+                            stdout=asyncio.subprocess.PIPE,
+                            stderr=asyncio.subprocess.STDOUT,
+                        )
+                    else:
+                        proc = await asyncio.create_subprocess_shell(
+                            f'ffmpeg -i "{path}" -c copy {crf}"{new_path}" -y',
+                            stdout=asyncio.subprocess.PIPE,
+                            stderr=asyncio.subprocess.STDOUT,
+                        )
+                    await proc.communicate()
+
+                    os.remove(path)
+                    path = new_path
+                    extension = "mp4"
+
+            thumb = path.replace(f".{extension}", ".jpg")
+            proc = await asyncio.create_subprocess_shell(
+                f'ffmpeg -ss 00:00:20 -i "{path}" -vf scale=320:-1 -vframes 1 -q:v 2 "{thumb}" -y',
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.STDOUT,
+            )
+            await proc.communicate()
+
+            if not os.path.exists(thumb):
+                thumb = (
+                    await self.bot.download_media(video.thumbs[0])
+                    if bool(video.thumbs) and len(video.thumbs) > 0
+                    else None
+                )
+
             video = (
                 await self.bot.send_video(
                     CHATS["videos"],
@@ -195,6 +195,8 @@ class VideoQueue(object):
 
             episode.update_from_dict({"file_id": video.file_id, "duration": duration})
             await episode.save()
+
+            shutil.rmtree(directory, ignore_errors=True)
         except BaseException as excep:
             text = "<b>Error processing an episode</b>\n"
             text += "\n<b>Anime</b>:"
@@ -211,8 +213,6 @@ class VideoQueue(object):
                 CHATS["staff"],
                 text,
             )
-
-        shutil.rmtree(directory, ignore_errors=True)
 
         if self.queue.empty() is False:
             await self.next()
